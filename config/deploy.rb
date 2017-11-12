163758dc95d5ef529da0dbafdac3c8ae94374e91
mrdShinse <shinse1128@hotmail.com>
2017-11-12 19:22:26 +0900

    setup capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,16 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.10.0"
+# frozen_string_literal: true
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+# config valid for current version and patch releases of Capistrano
+lock '~> 3.10.0'
+
+set :application, 'tour_concierge'
+set :repo_url, 'git@github.com:mrdShinse/tour_concierge.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, '/var/www/tour_concierge'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
