eb52dcefa01c5c9acf8b07c177d2d04ffdb43225
mrdShinse <shinse1128@hotmail.com>
2017-12-18 00:29:34 +0900

    implement job.

--- app/jobs/livefans/search_venues_list_job.rb
+++ app/jobs/livefans/search_venues_list_job.rb
@@ -4,6 +4,8 @@ module Livefans
   class SearchVenuesListJob < ApplicationJob # :nodoc:
     queue_as :livefans_search_venue_list
 
-    def perform(*args); end
+    def perform(*_args)
+      Livefans::Crawler.new.crawl_venues_list
+    end
   end
 end
