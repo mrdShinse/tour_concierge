1176988643bfae07453fcd3a6a27121c8baf1c12
mrdShinse <shinse1128@hotmail.com>
2017-12-27 22:33:45 +0900

    bugfix.

--- lib/livefans/events_list/crawlable.rb
+++ lib/livefans/events_list/crawlable.rb
@@ -11,7 +11,7 @@ module Livefans
         import_id = artist.import.split('/').last
         count = parse_events_list_crawling_count fetch_page(events_list_path(import_id, 1))
         data = count.times.map do |c|
-          parse_venues_list fetch_page(events_list_path(import_id, c + 1), import_id)
+          parse_events_list(fetch_page(events_list_path(import_id, c + 1)), import_id)
         end
         data.flatten.compact.each do |event|
           next if e.empty?
