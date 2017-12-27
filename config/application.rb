f6c0e2adb5905ab49d19fa710c7986bc2db8bc03
mrdShinse <shinse1128@hotmail.com>
2017-12-27 15:53:08 +0900

    add locale ja.

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,13 @@ module TourConcierge
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    I18n.enforce_available_locales = true
+    config.i18n.available_locales = %i[ja en]
+    config.i18n.default_locale = :ja
 
     # Don't generate system test files.
     config.generators do |g|
