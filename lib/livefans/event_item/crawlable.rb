46f047f9f54ce1fe7d14992825a012f8340b9a45
mrdShinse <shinse1128@hotmail.com>
2018-01-02 00:55:27 +0900

    bugfix.

--- lib/livefans/event_item/crawlable.rb
+++ lib/livefans/event_item/crawlable.rb
@@ -5,6 +5,8 @@ require Rails.root.join 'lib', 'livefans', 'url_helper'
 module Livefans
   module EventItem
     module Crawlable # :nodoc:
+      include Livefans::UrlHelper
+
       def crawl_event_item(event)
         html = fetch_page(event.import)
         data = parse_event_item html
@@ -16,15 +18,23 @@ module Livefans
 
       def parse_event_item(html)
         oga = Oga.parse_html(html)
-        { start_at: parse_start_at(oga) }
+        { start_at: parse_event_item_start_at(oga),
+          venue_id: parse_event_item_venue_id(oga) }
       rescue
         {}
       end
 
-      def parse_start_at(oga)
+      def parse_event_item_start_at(oga)
         text = oga.xpath('html/body/div/div/div/div/div/div/p')[0].text
         m = text.match(%r{(\d{4})/(\d{2})/(\d{2}).*(\d{2}):(\d{2}).*})
         Time.zone.local(m[1], m[2], m[3], m[4], m[5])
+      end
+
+      def parse_event_item_venue_id(oga)
+        v_import = oga.xpath('html/body/div/div/div/div/div/div/address/a')
+                      .attribute('href')[0]
+                      .value
+        ::Venue.find_by(import: "#{livefans_root_url}#{v_import}").id
       end
     end
   end
--- spec/livefans/event_item/crawlable_spec.rb
+++ spec/livefans/event_item/crawlable_spec.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 RSpec.describe Livefans::EventItem::Crawlable do
-  let(:target) { Class.include(described_class).new }
+  let(:target)  { Class.include(described_class).new }
   let(:fixture) { fetch_fixture events_fixture_path('event_item_865845.html') }
+  let!(:venue)  { ::Venue.create(name: '会場', import: 'http://www.livefans.jp/venues/2') }
 
   describe '#parse_event_item' do
     context 'correct htmol' do
       let(:html) { fixture }
       let(:result) do
-        { start_at: Time.zone.local(2017, 12, 29, 19, 30) }
+        { start_at: Time.zone.local(2017, 12, 29, 19, 30),
+          venue_id: venue.id }
       end
       subject { target.parse_event_item html }
 
