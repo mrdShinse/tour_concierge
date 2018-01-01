e79d5b193518871ee8c66d4628ac1b7a8e1e5d16
mrdShinse <shinse1128@hotmail.com>
2018-01-02 00:35:44 +0900

    implement crawling EventItem.

--- lib/livefans/event_item/crawlable.rb
+++ lib/livefans/event_item/crawlable.rb
@@ -7,8 +7,17 @@ module Livefans
     module Crawlable # :nodoc:
       include Livefans::UrlHelper
 
-      def parse_event_item(_html)
+      def parse_event_item(html)
+        oga = Oga.parse_html(html)
+        { start_at: parse_start_at(oga) }
+      rescue
         {}
+      end
+
+      def parse_start_at(oga)
+        text = oga.xpath('html/body/div/div/div/div/div/div/p')[0].text
+        m = text.match(%r{(\d{4})/(\d{2})/(\d{2}).*(\d{2}):(\d{2}).*})
+        Time.zone.local(m[1], m[2], m[3], m[4], m[5])
       end
     end
   end
