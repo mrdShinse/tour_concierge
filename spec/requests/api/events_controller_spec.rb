e4bca8e24fb6453d0e86c32d547c111eee82c1f0
mrdShinse <shinse1128@hotmail.com>
2018-01-07 19:27:43 +0900

    fix specification.

--- spec/requests/api/events_controller_spec.rb
+++ spec/requests/api/events_controller_spec.rb
@@ -20,16 +20,16 @@ RSpec.describe Api::EventsController, type: :request do
 
     context 'with lat-long' do
       context 'past events are not display.' do
-        let!(:venue)  { create(:venue, latitude: 100, longitude: 100) }
+        let!(:venue)  { create(:venue, latitude: 43, longitude: 144) }
         let!(:events) { create_list(:event, 2, venue: venue, start_at: Time.current.yesterday) }
 
-        let(:params) { { lat: 100, long: 100 } }
+        let(:params) { { q: { lat: 43, long: 144 } } }
         before { get api_events_nearby_path, params: params }
         subject { response }
 
         it { is_expected.to be_success }
         it { is_expected.to have_http_status(200) }
-        it 'have events' do
+        it 'have NO events' do
           body = JSON.parse(response.body)
           expect(body['ok']).to     eq 1
           expect(body['events']).to eq []
@@ -38,15 +38,15 @@ RSpec.describe Api::EventsController, type: :request do
 
       context 'far away events are not display.' do
         let!(:venue)  { create(:venue, latitude: 0, longitude: 0) }
-        let!(:events) { create_list(:event, 2, venue: venue, start_at: Time.current.yesterday) }
+        let!(:events) { create_list(:event, 2, venue: venue, start_at: Time.current.tomorrow) }
 
-        let(:params) { { lat: 100, long: 100 } }
+        let(:params) { { q: { lat: 100, long: 100 } } }
         before { get api_events_nearby_path, params: params }
         subject { response }
 
         it { is_expected.to be_success }
         it { is_expected.to have_http_status(200) }
-        it 'have events' do
+        it 'have NO events' do
           body = JSON.parse(response.body)
           expect(body['ok']).to     eq 1
           expect(body['events']).to eq []
@@ -54,10 +54,10 @@ RSpec.describe Api::EventsController, type: :request do
       end
 
       context 'nearby events are display.' do
-        let!(:venue)  { create(:venue, latitude: 100, longitude: 100) }
+        let!(:venue)  { create(:venue, latitude: 43, longitude: 144) }
         let!(:events) { create_list(:event, 2, venue: venue, start_at: Time.current.tomorrow) }
 
-        let(:params) { { lat: 100, long: 100 } }
+        let(:params) { { q: { lat: 43, long: 144 } } }
         before { get api_events_nearby_path, params: params }
         subject { response }
 
